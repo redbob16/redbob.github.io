df84d8c671bb2b4abaa7842aa42998a07abaa6b2
redbob16 <Troyer.psalms129@gmail.com>
2023-02-28 20:37:17 -0500

    Update functions.js

--- examples/js/functions.js
+++ examples/js/functions.js
@@ -15,15 +15,41 @@ function greetUser(username) {
     console.log(userGreeting);
 }
 
-greetUser("Landon");
-greetUser("Erica");
-greetUser("Ryan");
+greetUser("landon");
+greetUser("erica");
+greetUser("ryan");
 
 // function w input paramiter 
 // that modifies the page
 function greetUserOnPage(username) {
-    let greetingDisplay = document.getElementById(greetingDisplay);
+    let greetingDisplay = document.getElementById("greetingDisplay");
     greetingDisplay.innerHTML = greeting + ", " + username;
+
+    // greetingDisplay.innerHTML =
+    // greetingDisplay.innerHTML +
+    // greeting +
+    // ", " +
+    // username;
 }
 
-greetUserOnPage("Landon");
+greetUserOnPage("ryan");
+greetUserOnPage("landon");
+
+// function that outputs (or "returns") a value
+function getUserGreeting (username) {
+    return greeting + "! " + username + "! how can I help you";
+}
+
+getUserGreeting("landon");
+console.log(
+    getUserGreeting("landon")
+);
+
+// function using another function
+// that modifies the page
+function greetUserOnPageV2(username) {
+    let greetingDisplay = document.getElementById("greetingDisplay");
+    greetingDisplay.innerHTML = getUserGreeting(username);
+}
+
+greetUserOnPageV2("landon jay troyer");
