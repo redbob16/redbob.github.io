ff69129dd371e1217151920fb95ad49c5b1a0704
redbob16 <Troyer.psalms129@gmail.com>
2023-03-02 23:05:18 -0500

    updatedLightBox
    added Images

--- lightBox/js/lightBox.js
+++ lightBox/js/lightBox.js
@@ -13,7 +13,50 @@ function openLightbox() {
 }
 
 let coffeeImage = document.getElementById("coffeeImage");
-coffeeImage.onclick = openLightbox;
+    coffeeImage.onclick = openLightbox;
+
+let meOutside = document.getElementById("me");
+    meOutside.onclick = openLightbox;
+
+let rainbowBackground = document.getElementById("rainbowBackground");
+    rainbowBackground.onclick = openLightbox;
+
+let wNRS = document.getElementById("wNRS");
+    wNRS.onclick = openLightbox;
+
+let theUnfedMind = document.getElementById("theUnfedMind");
+    theUnfedMind.onclick = openLightbox;
+
+let coffeeLetTwo = document.getElementById("coffeeLetTwo");
+    coffeeLetTwo.onclick = openLightbox;
+
+let coffeeLiveThree = document.getElementById("coffeeLiveThree");
+    coffeeLiveThree.onclick = openLightbox;
+
+let coffeeLifeFour = document.getElementById("coffeeLifeFour");
+    coffeeLifeFour.onclick = openLightbox;
+
+let coffeePieOne = document.getElementById("coffeePieOne");
+    coffeePieOne.onclick = openLightbox;
+
+let coffeePoofTwo = document.getElementById("coffeePoofTwo");
+    coffeePoofTwo.onclick = openLightbox;
+
+let coffeePlayThree = document.getElementById("coffeePlayThree");
+    coffeePlayThree.onclick = openLightbox;
+
+let coffeePollyFour = document.getElementById("coffeePollyFour");
+    coffeePollyFour.onclick = openLightbox;
+
+let coffeeSideOne = document.getElementById("coffeeSideOne");
+    coffeeSideOne.onclick = openLightbox;
+
+let coffeeSaddleTwo = document.getElementById("coffeeSaddleTwo");
+    coffeeSaddleTwo.onclick = openLightbox;
+
+let coffeeSoupThree = document.getElementById("coffeeSoupThree");
+    coffeeSoupThree.onclick = openLightbox;
+
 
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
