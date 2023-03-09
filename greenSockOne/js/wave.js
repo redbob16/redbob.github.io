470dae0906e14b3af498453c4abbcee0ab833d2c
redbob16 <Troyer.psalms129@gmail.com>
2023-03-08 21:50:43 -0500

    greenSock2

---
+++ greenSockOne/js/wave.js
@@ -0,0 +1,14 @@
+gsap.to(".box", 0.5, {
+    scale: 0.1,
+    backgroundColor: "#8C8A68",
+    y: 60,
+    yoyo: true,
+    repeat: -1,
+    ease: "power1.inOut",
+    delay: 1,
+    stagger: {
+        amount: 10,
+        grid: "auto",
+        from: "center",
+    }
+});
